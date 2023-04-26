99e213d6538124a2271a750cdf04cd3cb733c41e
Kritik Jiyaviya <kritikjiyaviya07@gmail.com>
2023-04-26 09:18:02 +0530

    Fixes import error

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import express, {Router} from 'express';
-import {login, logout, register} from '../controllers/auth.controllers';
+import {login, logout, register} from '../controllers/auth.controllers.js';
 
 const router: Router = express.Router();
 
--- backend/src/utils/createSendToken.ts
+++ backend/src/utils/createSendToken.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from 'express';
-import {removeHash} from './password';
+import {removeHash} from './password.js';
 
 const createSendToken = (
   user: any,
