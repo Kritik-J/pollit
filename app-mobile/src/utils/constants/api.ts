1426cc7d4b34e8129cfb8bd53c4048b1208b9e81
Kritik Jiyaviya <kritikjiyaviya07@gmail.com>
2023-05-20 14:13:08 +0530

    api constants

--- app-mobile/src/utils/constants/api.ts
+++ app-mobile/src/utils/constants/api.ts
@@ -1,2 +1,2 @@
-export const API_VERSION = "v1";
-export const API_URL = `http://192.168.0.102:5000/api/${API_VERSION}`; //TODO: replace with .env variable later
+export const apiVersion = "v1";
+export const apiURL = `https://192.168.0.102:5000/api/${apiVersion}`;
